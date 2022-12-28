ff4d5c1c8275739ba2525ef06dcac72b757314d9
Alois T <alois.thibert@gmail.com>
2022-12-28 11:15:47 +0100

    requirements

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
-def add(x1:float, x2:float):
+def add(x1: float, x2: float):
     return x1 + x2
 
-def substract(x1:float, x2:float):
+
+def substract(x1: float, x2: float):
     return x1 - x2
--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,9 @@
 import calculator
 
+
 class TestCalculator:
-    
     def test_addition(self):
         assert calculator.add(x1=2, x2=2) == 4
-        
+
     def test_substraction(self):
         assert calculator.substract(x1=2, x2=2) == 0
-        
